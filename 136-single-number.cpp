6c9afbdfa59abd36fa881e1e2fc1b5809143e8e3
guochaofei <gcf1129@gmail.com>
2021-03-22 12:12:44 +0800

    fix allignment

--- 136-single-number.cpp
+++ 136-single-number.cpp
@@ -15,7 +15,7 @@ public:
 int Solution::singleNumber(std::vector<int>& nums)
 {
    return std::accumulate(nums.cbegin(), nums.cend(),
-                             0, std::bit_xor<int>());
+                          0, std::bit_xor<int>());
 }
 
 
