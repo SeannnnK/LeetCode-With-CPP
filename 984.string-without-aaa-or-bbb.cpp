efc18529d94420a77d59f80b0f2ae747bd4788b0
Sean Kuo <gcf1129@gmail.com>
2021-03-24 22:49:06 +0800

    recursive

---
+++ 984.string-without-aaa-or-bbb.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=984 lang=cpp
+ *
+ * [984] String Without AAA or BBB
+ */
+
+// @lc code=start
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string strWithout3a3b(int a, int b) {
+        if (a == 0) {
+            return string(b, 'b');
+        } else if (b == 0) {
+            return string(a, 'a');
+        } else if (a == b) {
+            return "ab" + strWithout3a3b(a - 1, b - 1);
+        } else if (a < b) {
+            return "abb" + strWithout3a3b(a - 1, b - 2);
+        } else {
+            return "aab" + strWithout3a3b(a - 2, b - 1);
+        }
+    }
+};
+// @lc code=end
+int main()
+{
+    Solution sol;
+    cout << sol.strWithout3a3b(4, 1) << endl;
+    return 0;
+}
