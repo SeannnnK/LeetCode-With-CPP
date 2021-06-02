456bb54589c2ded473a799a22a9b889ffe56c7f1
Sean Kuo <gcf1129@gmail.com>
2021-06-02 22:34:20 +0800

    two pointer

--- 167.two-sum-ii-input-array-is-sorted.cpp
+++ 167.two-sum-ii-input-array-is-sorted.cpp
@@ -5,19 +5,22 @@
  */
 
 // @lc code=start
-#include <vector>
+// amazon
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {
         int n = numbers.size();
         if (n < 2) {
             return vector<int>();
         }
-        for (int i = 0, j = n - 1; i != j; ) {
+        for (int i = 0, j = n - 1; i != j;) {
             if (numbers[i] + numbers[j] < target) {
                 ++i;
             } else if (numbers[i] + numbers[j] > target) {
@@ -43,4 +46,3 @@ int main()
     return 0;
 }
 // @lc code=end
-
