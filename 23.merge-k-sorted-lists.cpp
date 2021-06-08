3755c03771c69ddaad17c01cb54872b0ee3a7179
Sean Kuo <gcf1129@gmail.com>
2021-06-08 23:51:12 +0800

    priority_queue

--- 23.merge-k-sorted-lists.cpp
+++ 23.merge-k-sorted-lists.cpp
@@ -6,34 +6,47 @@
 
 // @lc code=start
 // airbnb | amazon | facebook | google | linkedin | microsoft | twitter | uber
-#include <vector>
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-struct compare {
-    bool operator() (ListNode *node1, ListNode* node2) {
-        return node1->val > node2->val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr)
+    {
+    }
+    ListNode(int x) : val(x), next(nullptr)
+    {
+    }
+    ListNode(int x, ListNode* next) : val(x), next(next)
+    {
     }
 };
 
 class Solution {
+    struct compare
+    {
+        bool operator()(ListNode* node1, ListNode* node2)
+        {
+            // min heap
+            return node1->val > node2->val;
+        }
+    };
+
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode *, vector<ListNode *>, compare> q;
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+        priority_queue<ListNode*, vector<ListNode*>, compare> q;
         ListNode dummy(0);
-        ListNode *node = &dummy;
+        ListNode* node = &dummy;
         for (const auto& list : lists) {
-            q.push(list);
+            if (list) {
+                q.push(list);
+            }
         }
         while (!q.empty()) {
             node->next = q.top();
@@ -49,16 +62,16 @@ public:
 
 int main()
 {
-    ListNode *n1 = new ListNode(1);
-    ListNode *n2 = new ListNode(4);
-    ListNode *n3 = new ListNode(5);
+    ListNode* n1 = new ListNode(1);
+    ListNode* n2 = new ListNode(4);
+    ListNode* n3 = new ListNode(5);
 
-    ListNode *n4 = new ListNode(1);
-    ListNode *n5 = new ListNode(3);
-    ListNode *n6 = new ListNode(4);
+    ListNode* n4 = new ListNode(1);
+    ListNode* n5 = new ListNode(3);
+    ListNode* n6 = new ListNode(4);
 
-    ListNode *n7 = new ListNode(2);
-    ListNode *n8 = new ListNode(6);
+    ListNode* n7 = new ListNode(2);
+    ListNode* n8 = new ListNode(6);
 
     n1->next = n2, n2->next = n3, n3->next = nullptr;
     n4->next = n5, n5->next = n6, n6->next = nullptr;
@@ -66,7 +79,7 @@ int main()
 
     vector<ListNode*> lists{n1, n4, n7};
     Solution sol;
-    ListNode *res = sol.mergeKLists(lists);
+    ListNode* res = sol.mergeKLists(lists);
 
     while (res) {
         cout << res->val << " ";
