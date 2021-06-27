0fcc1c69b6c09f74026ad37a621310a53506ba13
Sean Kuo <gcf1129@gmail.com>
2021-06-27 22:32:44 +0800

    binary search

--- 34.find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -6,14 +6,15 @@
 
 // @lc code=start
 // linkedin
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> searchRange(vector<int>& nums, int target)
+    {
         vector<int> res(2, -1);
         if (nums.empty()) {
             return res;
@@ -27,13 +28,14 @@ public:
     }
 
 private:
-    int lower_bound_index(vector<int>& nums, int target) {
+    int lower_bound_index(vector<int>& nums, int target)
+    {
         if (nums.empty()) {
             return -1;
         }
         int l = 0, r = nums.size() - 1;
         int mid = 0;
-         while (l + 1 < r) {
+        while (l + 1 < r) {
             mid = l + (r - l) / 2;
             if (nums[mid] < target) {
                 l = mid;
@@ -49,8 +51,9 @@ private:
             return -1;
         }
     }
-    
-    int upper_bound_index(vector<int>& nums, int target) {
+
+    int upper_bound_index(vector<int>& nums, int target)
+    {
         if (nums.empty()) {
             return -1;
         }
@@ -73,6 +76,68 @@ private:
         }
     }
 };
+
+
+class Solution1 {
+public:
+    vector<int> searchRange(vector<int>& nums, int target)
+    {
+        vector<int> res(2, -1);
+        if (nums.empty()) {
+            return res;
+        }
+        res[0] = lower_bound_index(nums, target);
+        if (res[0] == -1) {
+            return res;
+        }
+        res[1] = upper_bound_index(nums, target);
+        return res;
+    }
+
+private:
+    int lower_bound_index(vector<int>& nums, int target)
+    {
+        int low = 0, high = nums.size() - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    return mid;
+                }
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int upper_bound_index(vector<int>& nums, int target)
+    {
+        int low = 0, high = nums.size() - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                if (mid == nums.size() - 1 || nums[mid + 1] != target) {
+                    return mid;
+                }
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
 
 int main()
 {
