5b5f4e42922e0fe0498587285b6d6644b922a8bc
Sean Kuo <gcf1129@gmail.com>
2021-06-27 21:59:01 +0800

    binary search

--- 33.search-in-rotated-sorted-array.cpp
+++ 33.search-in-rotated-sorted-array.cpp
@@ -6,14 +6,15 @@
 
 // @lc code=start
 // bloomberg | facebook | linkedin | microsoft | uber
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target)
+    {
         // unique in nums
         int l = 0, r = nums.size() - 1;
         int mid = 0;
@@ -37,12 +38,52 @@ public:
                     l = mid;
                 }
             }
-
         }
         if (nums[l] == target) {
             return l;
         } else if (nums[r] == target) {
             return r;
+        }
+        return -1;
+    }
+};
+
+class Solution1 {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        // unique in nums
+        int l = 0, r = nums.size() - 1;
+        int mid = 0;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < nums[r]) {
+                // location is in the lesser part
+                if (nums[mid] <= target && target <= nums[r]) {
+                    // this is in order
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            } else if (nums[l] < nums[mid]) {
+                // location is in the larger part
+                if (nums[l] <= target && target <= nums[mid]) {
+                    // this is in order
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[l] == nums[mid]) {
+                    ++l;
+                }
+                if (nums[r] == nums[mid]) {
+                    --r;
+                }
+            }
         }
         return -1;
     }
